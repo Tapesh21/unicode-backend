5ef19338041e6f6cca5a35cf3d6d322c46f74cfb
Tapesh21 <89625758+Tapesh21@users.noreply.github.com>
2024-01-08 21:36:45 +0530

    Backend unicode

---
+++ src/main/java/com/unicode/backend/UnicodeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.unicode.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnicodeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnicodeBackendApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/unicode/backend/controller/UserController.java
@@ -0,0 +1,7 @@
+package com.unicode.backend.controller;
+
+public class UserController {
+	
+	String user = "Hello User";
+
+}
